7a8f6a6765ec7ad2ba2e4a0ea061d55383cef7ba
raseliet <rasele27@gmail.com>
2019-10-22 12:46:49 +0300

    FileDB data - Data array reiksmes priskyrimas setData - v1

--- classes/FileDB.php
+++ classes/FileDB.php
@@ -1,20 +1,20 @@
 <?php
 
 class FileDB {
-    
-    
+
     private $file_name;
     private $data;
-
 
     public function __construct($file_name) {
         $this->file_name = $file_name;
     }
-        public function load() {
-            $this->data = file_to_array($this->file_name);
-            
-            
-        
-    }
-}
 
+    public function load() {
+        $this->data = file_to_array($this->file_name);
+    }
+
+    public function setData($data_array) {
+        $this->data = $data_array;
+    }
+
+}
