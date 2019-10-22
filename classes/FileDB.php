5037d6d5286eb93f1f3b247eab47058222c378d0
raseliet <rasele27@gmail.com>
2019-10-22 22:32:39 +0300

    Add Row Get Row su komentarais - v1

--- classes/FileDB.php
+++ classes/FileDB.php
@@ -2,19 +2,44 @@
 
 class FileDB {
 
+    //nepasiekiamas metodas išorėje
     private $file_name;
     private $data;
 
+    //atviras metodas išorėje, t.y.prieinamas
+    //construct išsikviečia pats, tik sukūrus naują objektą
     public function __construct($file_name) {
         $this->file_name = $file_name;
     }
 
+    //pripildo objektą(užloadina), iš txt failo paima duomenis ir jsonu paverčia į array
     public function load() {
         $this->data = file_to_array($this->file_name);
     }
 
+    //įdedu data 
     public function setData($data_array) {
         $this->data = $data_array;
+    }
+
+    //returnina tai, kas nurodoma Ėthis->data
+    public function getData() {
+        return $this->data;
+    }
+
+    //įrašo į txt failą tai, kas tuo metu įdėta į variablą
+    public function save() {
+        return array_to_file($this->data, $this->file_name);
+    }
+
+    //paima vertę ir returnina
+    public function getrow($table, $row_id) {
+        return $this->data[$table][$row_id];
+    }
+
+    //nurodytu indeksu įdeda vertę, nieko nereturnina
+    public function addRow($table, $row) {
+        $this->data[$table][] = $row;
     }
 
 }
--- index.php
+++ index.php
@@ -4,6 +4,16 @@ require 'functions/file.php';
 require 'classes/FileDB.php';
 
 
-$FileDB = new FileDB('data/failas.txt');
+$db = new FileDB('data/db.txt');
 
-var_dump($FileDB);
+//pripildo objektą(užloadina), iš txt failo paima duomenis ir jsonu paverčia į array
+$db->load();
+
+//nurodytu indeksu įdeda vertę, nieko nereturnina
+$db->addRow('Vartotojai', ['name'=>'Rasa', 'surname' => 'Lietuvnikaite']);
+
+////array`ju issaugo i faila
+$db->save();
+
+$db->load();
+var_dump($db->getData());
